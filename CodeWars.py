fdd73a2f3c2b44aff742009c7c7db27a7baa6d4c
HakisIT <play.yaroslav@gmail.com>
2024-05-02 16:30:48 +0300

    Length of Last Word, Find the Index of the First Occurrence in a String

--- CodeWars.py
+++ CodeWars.py
@@ -156,3 +156,17 @@ def isValid(self, s):
         return True
     else:
         return False
+    
+
+# Length of Last Word
+# Given a string s consisting of words and spaces, return the length of the last word in the string.
+# A word is a maximal substring consisting of non-space characters only.
+def lengthOfLastWord(self, s):
+        return len(s.split()[-1])
+
+
+# Find the Index of the First Occurrence in a String
+# Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+def strStr(self, haystack, needle):
+        x = haystack.find(needle)
+        return x
